17c1f343d2debe995abda301cc155c1856c18582
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2023-12-29 18:10:09 +0100

    orgparse needs setuptools

--- flake.nix
+++ flake.nix
@@ -16,12 +16,22 @@
         # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
         pkgs = nixpkgs.legacyPackages.${system};
         inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication defaultPoetryOverrides;
+        pypkgs-build-requirements = {
+          orgparse = [ "setuptools" ];
+        };
+        p2n-overrides = defaultPoetryOverrides.extend (self: super:
+          builtins.mapAttrs (package: build-requirements:
+            (builtins.getAttr package super).overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg: if builtins.isString pkg then builtins.getAttr pkg super else pkg) build-requirements);
+            })
+          ) pypkgs-build-requirements
+        );
       in
         {
           packages = {
             myapp = mkPoetryApplication {
               projectDir = self;
-              overrides = defaultPoetryOverrides;
+              overrides = p2n-overrides;
             };
             default = self.packages.${system}.myapp;
           };
